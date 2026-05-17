cc3aa37a7fcd8d288bf948ab73ac3e1972817ec7
Yohan Brochard <yohan.brochard.44@gmail.com>
2026-05-17 14:52:42 +0200

    Ajout icones PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Les Poulettes du Marais',
+        short_name: 'Poulettes',
+        description: "Commande d'œufs frais",
+        theme_color: '#facc15',
+        background_color: '#fffbeb',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
